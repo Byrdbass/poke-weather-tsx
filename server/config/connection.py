73c04e1cf2ae0cf721882c8d1d98c91cf04b2027
ByrdBassPC <byrdbass@gmail.com>
2025-02-16 12:01:08 -0600

    created connection to database

---
+++ server/config/connection.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+HOST = os.getenv("DB_HOST_NAME")
+USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password"
+)
+
+if mydb.is_connected():
+    print("CONNECTED TO MYSQL")
+    mydb.close()
+else:
+    print("failed to connect to DB")
